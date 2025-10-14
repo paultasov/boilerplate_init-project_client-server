3196af79dd16b275334790dde864470b9f776f77
paultasov <paultasov@gmail.com>
2025-10-14 13:20:04 +0300

    chore: project init

---
+++ server/src/server.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+
+const PORT = process.env.SERVER_PORT ?? 3000;
+
+app.listen(PORT, () => {
+  try {
+    console.log(`Server started on PORT -- ${PORT} --`);
+  } catch (err) {
+    console.log(err);
+  }
+});
