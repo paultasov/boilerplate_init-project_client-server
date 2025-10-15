29643cafb9c743e7a6e38c0a263ca0c0d6441b99
Pavel T. <paultasov@gmail.com>
2025-10-15 21:56:29 +0300

    Update app.js - add morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 
 // Парсинг тела HTTP-запросов с типом application/x-www-form-urlencoded
@@ -11,5 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // После подключения можно читать cookies из запроса через req.cookies
 app.use(cookieParser());
+
+// routes go here...
 
 module.exports = app;
